ec2fba9fc4305ef31fc431a1df75528c283e26a2
JCGJ94 <jcgonj1994@gmail.com>
2025-10-29 22:01:32 +0000

    update pipfile

--- src/api/admin.py
+++ src/api/admin.py
@@ -10,7 +10,7 @@ from .models import User
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
     app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
+    admin = Admin(app, name='JC_DEV Admin')
 
     # Add your models here, for example this is how we add the User model to the admin
     admin.add_view(ModelView(User, db.session))
